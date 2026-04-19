3aa4944ee5910e8656e6703420583618aa963357
geraldoairessjr-svg <geraldoairessjr@gmail.com>
2026-04-19 14:46:13 -0300

    Update iniciar.bat

--- iniciar.bat
+++ iniciar.bat
@@ -1,29 +1,5 @@
 @echo off
 cd /d "%~dp0"
-setlocal
-
-echo =========================
-echo Baixando FFMPEG.zip...
-echo =========================
-
-REM Caminho da pasta onde o .bat está
-set "PASTA=%~dp0"
-
-REM URL do arquivo
-set "URL=https://github.com/geraldoairessjr-svg/tf777_robot/releases/download/ZIP/FFMPEG.zip"
-
-REM Nome do arquivo de saída
-set "ARQUIVO=%PASTA%FFMPEG.zip"
-
-REM Download usando PowerShell
-powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%ARQUIVO%'"
-
-if exist "%ARQUIVO%" (
-echo Download concluido com sucesso!
-) else (
-echo ERRO ao baixar o arquivo.
-)
-tar -xf "FFMPEG.zip"
 echo Iniciando...
 python main.py
 pause
